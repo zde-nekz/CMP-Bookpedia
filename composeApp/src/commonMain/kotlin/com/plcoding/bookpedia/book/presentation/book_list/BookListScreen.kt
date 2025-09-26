8c4a5291c3be6ba6312d2dd6317cc29df515dc19
Zdenek Skrobak <z.skrobak@hotmail.cz>
2025-09-26 14:41:26 +0200

    Domain, searchBar 1:02:09

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -0,0 +1,66 @@
+package com.plcoding.bookpedia.book.presentation.book_list
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.components.SearchBar
+import com.plcoding.bookpedia.core.presentation.DarkBlue
+
+@Composable
+fun BookListScreenRoot(
+    viewModel: BookListViewModel,
+    onBookClick: (Book) -> Unit,
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    BookListScreen(
+        state = state,
+        onAction = { action ->
+            when (action) {
+                is BookListAction.OnBookClick -> onBookClick(action.book)
+                else -> viewModel.onAction(action)
+            }
+        })
+}
+
+@Composable
+private fun BookListScreen(
+    state: BookListState,
+    onAction: (BookListAction) -> Unit
+) {
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DarkBlue)
+            .statusBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SearchBar(
+            modifier = Modifier
+                .widthIn(max = 400.dp)
+                .padding(16.dp),
+            searchQuery = state.searchQuery,
+            onSearchQueryChange = {
+                onAction(BookListAction.OnSearchQueryChange(it))
+            },
+            onImeSearch = {
+                keyboardController?.hide()
+            },
+
+            )
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -0,0 +1,13 @@
+package com.plcoding.bookpedia.book.presentation.book_list
+
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.core.presentation.UiText
+
+data class BookListState(
+    val searchQuery: String = "Kotlin",
+    val searchResults: List<Book> = emptyList(),
+    val favoriteBooks: List<Book> = emptyList(),
+    val isLoading: Boolean = false,
+    val selectedTabIndex: Int = 0,
+    val errorMessage: UiText? = null
+)
